55a116322b2085882f6a7c128f60870b15b32540
MeesCode <m.brinkhuis@student.tudelft.nl>
2021-01-20 22:58:08 +0100

    headless mode

--- bot.js
+++ bot.js
@@ -1,26 +1,45 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-var fetch = require('node-fetch');
+const Discord = require('discord.js');
+const auth = require('./auth.json');
+const fetch = require('node-fetch');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
+const client = new Discord.Client();
+client.login(auth.token);
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+const helpMessage = `\`\`\`$join to enter your voice chat
+$leave to leave the voicechat
+$search to perform a search query (max 10)
+$add <number> to add a search result to the playlist
+$skip to go to the next song
+$queue to see to current queue\`\`\``
+const root = process.argv[2]
+
+let curTextChannel = null
+let connection = null
+
+// variables to hold the playlist search results
+let playlist = []
+let searchresults = []
+
+client.on('ready', function (evt) {
+    console.log("bot started")
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+function play(){
+    console.log(playlist)
+    if(playlist.length === 0){
+        curTextChannel.send("playlist is empty")
+        return
+    }
+    connection.play(root + playlist[0].Path).on("finish", () => 
+        nextSong()
+    );
+    curTextChannel.send(`Now playing:\`\`\`${trackToTitle(playlist[0])}\`\`\``)
+}
+
+function nextSong(){
+    playlist.shift()
+    play()
+}
 
 function trackToTitle(data){
     if(data.Title.Valid && data.Artist.Valid)
@@ -29,24 +48,21 @@ function trackToTitle(data){
         return data.Path
 }
 
-bot.on('message', function (user, userID, channelID, message, evt) {
+client.on('message', message => {
 
-    if (message.substring(0, 1) == '$') {
-        var args = message.substring(1).split(' ');
+    curTextChannel = message.channel
+
+    if (message.content.substring(0, 1) == '$') {
+
+        var args = message.content.substring(1).split(' ');
         var cmd = args[0];
-       
         args = args.splice(1);
+
         switch(cmd) {
+
             case 'help':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 
-`\`\`\`$search to perform a search query (max 10)
-$add <number> to add a search result to the playlist
-$playpause to play or pause
-$skip to go to the next song
-$queue to see to current queue\`\`\``
-                });
+                console.log(root)
+                message.channel.send(helpMessage)
             break;
 
             case 'search':
@@ -54,104 +70,74 @@ $queue to see to current queue\`\`\``
                 .then(res => res.json())
                 .then(data => {
                     if(Object.entries(data).length == 0){
-                        bot.sendMessage({
-                            to: channelID,
-                            message: 'No entries found :('
-                        });
+                        message.channel.send('No entries found :(')
                         return
                     }
-                    let message = "Add to playlist by entering `$add <number>`: ```"
+                    searchresults = data
+                    let m = "Add to playlist by entering `$add <number>`: ```"
                     for(let [k, v] of Object.entries(data)){
-                        message += `${k}. ${trackToTitle(v)}\n`
+                        m += `${k}. ${trackToTitle(v)}\n`
                     }
-                    bot.sendMessage({
-                        to: channelID,
-                        message: message+'```'
-                    });
+                    m += '```'
+                    message.channel.send(m)
                 })
                 .catch(res => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Something went wrong\n\n${res}`
-                    });
+                    message.channel.send(`Something went wrong\n\n${res}`)
                 })
             break;
             
             case 'skip':
-                fetch('http://localhost:8080/skip')
-                .then(res => res.json())
-                .then(data => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Now playing: \`\`\`${trackToTitle(data)}\`\`\``
-                    });
-                })
-                .catch(res => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Something went wrong\n\n${res}`
-                    });
-                })
+                message.channel.send("skipping track")
+                nextSong()
             break;
 
-            case 'playpause':
-                fetch('http://localhost:8080/playpause')
-                .then(res => res.json())
-                .then(data => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Play/pause while playing: \`\`\`${trackToTitle(data)}\`\`\``
-                    });
-                    
-                })
-                .catch(res => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Something went wrong\n\n${res}`
-                    });
-                })
+            case 'stop':
+            case 'leave':
+                playlist = []
+                if(connection != null) connection.disconnect()
             break;
 
             case 'add':
-                fetch(`http://localhost:8080/add?query=${encodeURI(args.join(" "))}`)
-                .then(res => res.json())
-                .then(data => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Added: \`\`\`${trackToTitle(data)}\`\`\``
-                    });
-                })
-                .catch(res => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Something went wrong\n\n${res}`
-                    });
-                })
+                n = parseInt(args[0], 10)
+                if (n == NaN) {
+                    message.channel.send('Not a valid number')
+                    return
+                }
+                if (n < 0 || n > searchresults.length - 1){
+                    message.channel.send('Number not in last search results')
+                    return
+                }
+                playlist.push(searchresults[n])
+                message.channel.send(`Added to playlist:\`\`\`${trackToTitle(searchresults[n])}\`\`\``)
+                if(playlist.length === 1){
+                    play()
+                }
+            break;
+
+            case 'join':
+                if (!message.member.voice.channel) return message.channel.send("You must be in a voice channel.");
+                if (message.guild.me.voice.channel) {
+                    message.channel.send("I'm already here");
+                }
+
+                message.member.voice.channel.join().then(c => {
+                    connection = c
+                    message.channel.send(helpMessage);
+                }).catch(e => console.log(e))
+
             break;
 
             case 'queue':
-                fetch(`http://localhost:8080/queue`)
-                .then(res => res.json())
-                .then(data => {
-                    let message = "now in the queue: ```"
-                    for(let [k, v] of Object.entries(data)){
-                        if (k == 0) {
-                            message += `${k}.\t(playing) ${trackToTitle(v)}\n`
-                        } else {
-                            message += `${k}.\t${trackToTitle(v)}\n`
-                        }
+                let m = "now in the queue: ```"
+                for(let [k, v] of Object.entries(playlist)){
+                    if (k == 0) {
+                        m += `${k}.\t(playing) ${trackToTitle(v)}\n`
+                    } else {
+                        m += `${k}.\t${trackToTitle(v)}\n`
                     }
-                    bot.sendMessage({
-                        to: channelID,
-                        message: message+'```'
-                    });
-                })
-                .catch(res => {
-                    bot.sendMessage({
-                        to: channelID,
-                        message: `Something went wrong\n\n${res}`
-                    });
-                })
+                }
+                m += '```'
+                message.channel.send(m)
             break;
 
 
